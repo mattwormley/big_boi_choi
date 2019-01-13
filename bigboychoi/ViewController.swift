182ca6d892b29f6a6a8280c0a4b0c09bb79fc764
Matt Wormley <mwormley@adobe.com>
2019-01-12 19:04:44 -0800

    innings

--- bigboychoi/ViewController.swift
+++ bigboychoi/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var inningLabels:[(top: UILabel, bottom: UILabel)] = []
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for i in 1...9 {
+            
+            let inningTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
+            
+            let labels = (top: UILabel(frame: CGRect(x: 0, y: 0, width: 25, height: 25)),
+                          bottom: UILabel(frame: CGRect(x: 0, y: 0, width: 25, height: 25)));
+            
+            inningTitle.center = CGPoint(x: 80 + i * 26, y: 574)
+            inningTitle.textAlignment = .center
+            inningTitle.text = String(i)
+            self.view.addSubview(inningTitle)
+            
+            labels.top.center = CGPoint(x: 80 + i * 26, y: 600)
+            labels.top.textAlignment = .center
+            labels.top.text = "0"
+            self.view.addSubview(labels.top)
+            
+            labels.bottom.center = CGPoint(x: 80 + i * 26, y: 626)
+            labels.bottom.textAlignment = .center
+            labels.bottom.text = "0"
+            self.view.addSubview(labels.bottom)
+            
+            inningLabels += [labels]
+            
+        }
+        
+        inningLabels[3].top.text = "4"
+
     }
 
     override func didReceiveMemoryWarning() {
